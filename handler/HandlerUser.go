156713e98d99579d40851492e044a1f12e612642
Fathur Rohman <fr081938@gmail.com>
2024-11-21 05:57:03 +0700

    [fix] remove unused code

--- handler/HandlerUser.go
+++ handler/HandlerUser.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"SecretCare/entity"
-	"SecretCare/utils"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,12 +13,6 @@ type HandlerUser interface {
 
 func (h *handler) GetUserByUsername(username string) (*entity.Users, error) {
 	var user entity.Users
-	fmt.Print(h.ctx)
-	users, ok := utils.GetUserFromContext(h.ctx)
-	if ok {
-		fmt.Print(users.FullName, users.ID, "test 123213")
-	}
-
 	row := h.db.QueryRow("SELECT id, username, full_name, role, password FROM users WHERE username = ?", username)
 
 	if err := row.Scan(&user.ID, &user.Username, &user.FullName, &user.Role, &user.Password); err != nil {
