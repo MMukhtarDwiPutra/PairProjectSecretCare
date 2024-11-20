592c6a2181ba760c944a269ade247182f190b5f8
Fathur Rohman <fr081938@gmail.com>
2024-11-21 06:38:49 +0700

    [feat] add UpdateMyAccount handler

--- handler/HandlerUser.go
+++ handler/HandlerUser.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"SecretCare/entity"
+	"SecretCare/utils"
+	"context"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -9,6 +11,8 @@ import (
 
 type HandlerUser interface {
 	GetUserByUsername(username string) (*entity.Users, error)
+	DeleteMyAccount(userId int) error
+	UpdateMyAccount(username, password, fullName string) error
 }
 
 func (h *handler) GetUserByUsername(username string) (*entity.Users, error) {
@@ -20,4 +24,47 @@ func (h *handler) GetUserByUsername(username string) (*entity.Users, error) {
 	}
 
 	return &user, nil
+}
+
+func (h *handler) UpdateMyAccount(username, password, fullName *string) (context.Context, error) {
+	user, ok := utils.GetUserFromContext(h.ctx)
+	if !ok {
+		return h.ctx, fmt.Errorf("user not found in context")
+	}
+
+	query := "UPDATE users SET "
+	params := []interface{}{}
+
+	if username != nil {
+		query += "username = ?, "
+		params = append(params, *username)
+	}
+	if password != nil {
+		query += "password = ?, "
+		params = append(params, *password)
+	}
+	if fullName != nil {
+		query += "full_name = ?, "
+		params = append(params, *fullName)
+	}
+
+	query = query[:len(query)-2]
+	query += " WHERE id = ?"
+	params = append(params, user.ID)
+
+	_, err := h.db.Exec(query, params...)
+
+	newUpdatedUser := &entity.Users{ID: user.ID}
+	if username != nil {
+		newUpdatedUser.Username = *username
+	}
+	if fullName != nil {
+		newUpdatedUser.FullName = *fullName
+	}
+
+	h.ctx = utils.SetUserInContext(h.ctx, newUpdatedUser)
+	if err != nil {
+		return h.ctx, fmt.Errorf("failed to update user: %w", err)
+	}
+	return h.ctx, nil
 }
